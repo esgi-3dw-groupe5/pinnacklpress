f9ff0f412ec106cf6f3b1f16f8dd9d618c1da6e0
Syu93 <herve.tutuaku@gmail.com>
2015-05-14 22:31:40 +0200

    [bugfix] adding renamed files part of linux build

---
+++ sophwork/app/controller/AppController.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ *	This file is a part of the sophwork project
+ *	@Tested version : Sophwork.0.2.2
+ *	@author : Syu93
+ *	--
+ *	Main controller class
+ */
+
+namespace sophwork\app\controller;
+
+use sophwork\core\Sophwork;
+use sophwork\app\app\SophworkApp;
+	// KDM
+use sophwork\modules\kdm\SophworkDM;
+use sophwork\modules\kdm\SophworkDMEntities;
+	// KTE
+use sophwork\modules\kte\SophworkTELoader;
+use sophwork\modules\kte\SophworkTELexer;
+use sophwork\modules\kte\SophworkTEParser;
+
+class AppController extends SophworkApp{
+	protected $action;
+	// FIXME : Get all these param dynamicaly
+	protected $page;
+	protected $article;
+
+	protected $loader;
+	protected $template;
+	protected $KTE;
+	protected $data;
+
+	public $appModel;
+	protected $KDM;
+
+	public function __construct($appModel = null){
+		parent::__construct();
+		// FIXME : Get all these param dynamicaly
+		$this->page 	= Sophwork::getParam('p','index');
+		$this->article 	= Sophwork::getParam('a','');
+		
+		$this->appModel = $appModel;
+
+		$action = $this->setConfigAction($_POST);
+		$this->sendConfigAction($action, $_POST);
+
+		if(!Sophwork::getConfig()){
+			if($this->page == 'index')
+				Sophwork::redirect('config');
+			// use KTE to render the template
+			$loader = new SophworkTELoader();
+			$template = $loader->loadFromFile("template/".$this->page.".tpl");
+			$KTE = new SophworkTEParser($template,$data = [
+				'title' =>  'Setup config file',
+				'h1' =>  'Setup config file',	
+				'header' =>  'Setup config file',
+				'base' => 'template/css/base/base.css',
+				'forms' => 'template/css/forms/forms.css',
+				'buttons' => 'template/css/buttons/buttons.css','template/css/buttons/buttons.css',
+			]);
+			echo $KTE->parseTemplate();
+			exit;
+		}
+		else{
+			if($this->page == 'config') {
+				$this->KDM = new SophworkDM(Sophwork::getConfig());
+				include('database/sophkdatabase.php');
+				Sophwork::redirect();
+			}
+		}
+
+		$this->KDM = new SophworkDM(Sophwork::getConfig());
+	}
+
+	public function __get($param){
+		if(isset($this->$param))
+			return $this->$param;
+		return false;
+	}
+
+	public function __set($param, $value){
+		$this->$param = $value;
+	}
+
+	public function getDataFromModel(){
+		return $this->appModel->data;
+	}
+
+	public function setConfigAction($POST){
+		if(array_key_exists('db_submit',$POST))
+    		return $this->action = 'db_submit';
+	}
+
+	public function sendConfigAction($action,$POST){
+		if ($action == 'db_submit'){
+			Sophwork::setConfig($POST);
+			Sophwork::redirect();
+		}
+	}
+}
---
+++ sophwork/app/model/AppModel.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ *	This file is a part of the sophwork project
+ *	@Tested version : Sophwork.0.2.2
+ *	@author : Syu93
+ *	--
+ *	Main model class
+ */
+
+namespace sophwork\app\model;
+
+use sophwork\app\app\SophworkApp;
+
+class AppModel extends SophworkApp{
+
+    public $config;
+	protected $data;
+    protected $link;
+
+	public function __construct($config = null){
+        if(!is_null($config) && $config != false)
+            $this->config = $config;
+        else
+            $this->config = [
+                'db_host' => null,
+                'db_name' => null,
+                'db_login' => null,
+                'db_password' => null,
+            ];
+        $this->link = $this->connectDatabase();
+	}
+
+	public function __get($param){
+		return $this->$param;
+	}
+
+	public function __set($param, $value){
+        $this->$param = $value;
+	}
+
+	public function connectDatabase(){
+        extract($this->config);
+		try{
+			$link = new \PDO('mysql:host='.$db_host.';dbname='.$db_name,$db_login,$db_password,
+			array(
+				\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+				\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+			));
+		}
+		catch(Exception $e){
+			die("Erreur : ".$e->getMessage());
+		}
+		return $link;
+	}
+    
+    /**
+     *  deprecated !
+     */
+    public function createDatabase($host, $root, $root_pswd, $user, $user_pswd, $new_db)
+    {
+        try {
+            $db = new \PDO("mysql:host=$host", $root, $root_pswd);
+
+            $db->exec("CREATE DATABASE `$new_db`;
+                CREATE USER '$user'@'$host' IDENTIFIED BY '$pass';
+                GRANT ALL ON `$db`.* TO '$user'@'$host';
+                FLUSH PRIVILEGES;") 
+                or die(print_r($db->errorInfo(), true));
+
+        } catch (\PDOException $e) {
+            die("DB ERROR: ". $e->getMessage());
+        }
+    }
+}
