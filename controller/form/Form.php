024feeddb85066071c40f6078666025a08d30c8d
athorel <alexis.thorel@gmail.com>
2015-07-04 13:16:39 +0200

    [bugfix] duplicate error on validation form

--- controller/form/Form.php
+++ controller/form/Form.php
@@ -34,13 +34,21 @@ class Form extends \sophwork\app\controller\AppController{
 		$fieldArray = $field->getFields($data['field_id'],$this->KDM);
 		// Search in the rs field table for the field link to validator
 		$rsField = $this->KDM->create('pp_field_rs');
+
+		$field = 0;
+		$validatorId = 0;
 		for($i = 0 ; $i < count($data['field_id']) ; $i++){
 			$rsField->findFieldId($data['field_id'][$i]);
 			$dataField = $rsField->getData();
+			
 			for($j = 0 ; $j < count($dataField['validator_id']) ; $j++) {
-				$tempArray['field_id'] = $dataField['field_id'][$j];
-				$tempArray['validator_id'] = $dataField['validator_id'][$j];
-				$this->fields[] = $tempArray;
+				if($field !=  $dataField['field_id'][$j] && $validator != $dataField['validator_id'][$j]){
+					$tempArray['field_id'] = $dataField['field_id'][$j];
+					$tempArray['validator_id'] = $dataField['validator_id'][$j];
+					$this->fields[] = $tempArray;
+				}
+				$field = $dataField['field_id'][$j];
+				$validatorId = $dataField['validator_id'][$j];
 			}			
 		}
 		$formArray = $validator->getRules($this->fields,$this->KDM,$fieldArray);
