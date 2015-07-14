b4d6fb0b5b7a3cb5d859a15b4d583cdab0869805
athorel <alexis.thorel@gmail.com>
2015-07-15 00:25:04 +0200

    [bugfix] avoid empty case rss

--- controller/posts/fluxRss.php
+++ controller/posts/fluxRss.php
@@ -24,22 +24,26 @@ function update_fluxRSS(){
         $posts = new Post();
         $data = $posts->getPosts();
 
-        foreach ($data['title'] as $k => $value) {
+        if(!empty($data)){
+               foreach ($data['title'] as $k => $value) {
 
-                $title   = $data['title'][$k];
-                $content = substr($data['content'][$k], 0,100);
-                $author  = $data['author'][$k];
-                $tag     = $data['tag'][$k];
-                $date    = $data['date'][$k];
+                        $title   = $data['title'][$k];
+                        $content = substr($data['content'][$k], 0,100);
+                        $author  = $data['author'][$k];
+                        $tag     = $data['tag'][$k];
+                        $date    = $data['date'][$k];
 
-                $xml .= '<item>';
-                $xml .= '<title>'.'['.$data['category'][$k][0].']'.stripcslashes($title).'</title>';
-                $xml .= '<link>'.Sophwork::getUrl($tag).'</link>';
-                $xml .= '<guid isPermaLink="true">'.Sophwork::getUrl($tag).'</guid>';
-                $xml .= '<pubDate>'.(date("D, d M Y H:i:s O", strtotime($date))).'</pubDate>';
-                $xml .= '<author>'.stripcslashes($author).'</author>';
-                $xml .= '</item>'; 
+                        $xml .= '<item>';
+                        $xml .= '<title>'.'['.$data['category'][$k][0].']'.stripcslashes($title).'</title>';
+                        $xml .= '<link>'.Sophwork::getUrl($tag).'</link>';
+                        $xml .= '<guid isPermaLink="true">'.Sophwork::getUrl($tag).'</guid>';
+                        $xml .= '<pubDate>'.(date("D, d M Y H:i:s O", strtotime($date))).'</pubDate>';
+                        $xml .= '<author>'.stripcslashes($author).'</author>';
+                        $xml .= '</item>'; 
+                } 
         }
+
+        
 
         $xml .= '</channel>';
         $xml .= '</rss>';
